b11850fef40cc2aabce49f49b5ee126d1cd2ac26
Bui Phi Quan <rongxanh2052004@yahoo.com>
2013-03-16 12:20:46 +0700

    deploy code

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,29 +9,29 @@ set :use_sudo, false
 ssh_options[:keys] = ["#{ENV['HOME']}/.ssh/bpquanpublic.pem"]
 
 set :scm, :git
-set :repository, "git@github.com:markofevil3/smartjsc.git"
+set :repository, "git@github.com:markofevil3/cokhibachkhoa.git"
 set :branch, "stable"
 set :repository_cache, "git_cache"
 set :deploy_via, :remote_cache
 # set :ssh_options, { :forward_agent => true }
 
-set :smartjsc_server, "ec2-54-251-73-184.ap-southeast-1.compute.amazonaws.com"
-role :app, smartjsc_server
+set :cokhibachkhoa_server, "ec2-54-251-73-184.ap-southeast-1.compute.amazonaws.com"
+role :app, cokhibachkhoa_server
 
-namespace :smartjsc do
-  after "smartjsc:update", "smartjsc:restart"
+namespace :cokhi do
+  after "cokhi:update", "cokhi:restart"
 
   task :update, :roles => :app do
-    run "cd /home/ec2-user/www/smartjsc; git pull"
+    run "cd /home/ec2-user/www/cokhibachkhoa; git pull"
   end
 
   task :start, :roles => :app do
-    run "cd /home/ec2-user/www/smartjsc; mongod --dbpath ~/mongodb/data >> production.log 2>&1 &"
-    run "cd /home/ec2-user/www/smartjsc; ./start.sh >> production.log 2>&1 &"
+    run "cd /home/ec2-user/www/cokhibachkhoa; mongod --dbpath ~/mongodb/data >> production.log 2>&1 &"
+    run "cd /home/ec2-user/www/cokhibachkhoa; sudo node app.js >> production.log 2>&1 &"
   end
 
   task :stop, :roles => :app do
-    run "cd /home/ec2-user/www/smartjsc; sudo pkill node >> production.log 2>&1 &"
+    run "cd /home/ec2-user/www/cokhibachkhoa; sudo pkill node >> production.log 2>&1 &"
   end
 
   task :restart, :roles => :app do
